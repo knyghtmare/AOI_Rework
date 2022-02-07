996c40ef3a38179d649b22dfec3308a1da152efc
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-07 22:04:15 +0600

    Update scenario_01.lua

--- lua/scenario_01.lua
+++ lua/scenario_01.lua
@@ -32,3 +32,74 @@ on_event("start", function()
         message = _"We will see if the darkness is your ally, monsters from the sea!", 
     }
 end)
+
+-- when all side 3 units are dead
+on_event("die", function()
+    -- filter for side 3
+    if wesnoth.wml_conditionals.have_unit { side = "3"} then
+		return
+	end
+    wesnoth.wml_actions.message {
+        speaker = "Sunllis",
+        message = _"But you have heard and seen, there are many more of them than we thought, and more and more injured ones can come in the next fights.",
+    }
+    wesnoth.wml_actions.message {
+        speaker = "Erlornas",
+        message = _"We must count and only we can count on our perseverance and the will that the light of our righteous hearts illuminate the darkness that begins to contaminate the house we have always lived in.",
+    }
+    wesnoth.wml_actions.message {
+        speaker = "Sunllis",
+        message = _"Tell me, do you ... (He looks at Erlornas, as he clean his sword of orcs blood against the body of his enemy, and wonders whether to ask)",
+    }
+    wesnoth.wml_actions.message {
+        speaker = "Erlornas",
+        message = _"What is it, is something bothering you?",
+    }
+    wesnoth.wml_actions.message {
+        speaker = "Sunllis",
+        message = _"No, it's nothing ... (Thinks)",
+    }
+    wesnoth.wml_actions.message {
+        speaker = "Sunllis",
+        message = _"(Looks into the distance) These beasts will only be more numerous and even more powerful, and what about the borders, when their scouts reach us already so far?",
+    }
+    wesnoth.wml_actions.message {
+        speaker = "Sunllis",
+        message = _"The darkness is getting bigger and our endurance has its limits ...",
+    }
+    wesnoth.wml_actions.message {
+        speaker = "Erlornas",
+        message = _"However, I will not stop, you yourself saw those unfortunates who were murdered by those dark children with cold blood only to sow the seeds of fear in us.",
+    }
+    wesnoth.wml_actions.message {
+        speaker = "Sunllis",
+        message = _"Maybe it is better to wait for the support that may come soon?",
+    }
+    wesnoth.wml_actions.message {
+        speaker = "Erlornas",
+        message = _"What support? Are you trying to build up your illusions? We have been sent to death from the beginning. (Everyone around is watching with fear)",
+    }
+    wesnoth.wml_actions.message {
+        speaker = "Erlornas",
+        message = _"Yes ... We all will die, so we had three days to leave, to say goodbye to those we love for the last time. (Looks at his ring)",
+    }
+    wesnoth.wml_actions.message {
+        speaker = "Erlornas",
+        message = _"I don't expect to see her again ... (You can see a tear in his eye)",
+    }
+    wesnoth.wml_actions.message {
+        speaker = "Sunllis",
+        message = _"Erlornas you ...",
+    }
+    wesnoth.wml_actions.message {
+        speaker = "Erlornas",
+        message = _"(He turns towards her and, not wanting to show his weakness, he rubs his eyes with his cape) So let's move on ...",
+    }
+    wesnoth.wml_actions.endlevel {
+        result = "victory",
+        bonus = "no",
+        carryover_report = "no",
+        linger_mode = "yes",
+        carryover_percentage = "0",
+    }
+end)
