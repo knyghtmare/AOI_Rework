480b8c1e24bd0e084805a5b6d4d64f91920cfc61
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-07 19:33:04 +0600

    Update scenario_01.lua

--- lua/scenario_01.lua
+++ lua/scenario_01.lua
@@ -1,3 +1,18 @@
 local _ = wesnoth.textdomain 'wesnoth-AOI_Rework'
 local T = wml.tag
 local on_event = wesnoth.require("on_event")
+
+on_event("start", function()
+    wesnoth.wml_actions.message {
+        speaker = "Sunllis",
+        message = _"Their darkness could be felt already a mile away, they look wild, but war is their job and they were made to war.", 
+    }
+    wesnoth.wml_actions.message {
+        speaker = "Erlornas",
+        message = _"Dirty creatures, children of envy and evil from their first day of birth have been named!", 
+    }
+    wesnoth.wml_actions.message {
+        speaker = "OrcLeader1",
+        message = _"Dirty creatures, children of envy and evil from their first day of birth have been named!", 
+    }
+end)
